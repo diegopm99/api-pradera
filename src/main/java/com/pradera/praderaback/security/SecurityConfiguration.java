fc4a273a173331fe171666ea5ad572917dc97989
Diego Anfer Perez Mendoza <diegoaperezm.dp@gmail.com>
2024-07-16 15:29:40 -0500

    actualizacion de jar

--- src/main/java/com/pradera/praderaback/security/SecurityConfiguration.java
+++ src/main/java/com/pradera/praderaback/security/SecurityConfiguration.java
@@ -12,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +37,13 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.cors().configurationSource(request -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("*"));
+            cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+        });
         http.csrf()
                 .disable()
                 .exceptionHandling()
